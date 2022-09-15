5ed4f4997583615292c9161660ba398f424cb48a
Francesc <francesc.denti@gmail.com>
2022-09-15 10:09:47 +0200

    Implementación Interface Movable

---
+++ src/main/java/edu/uoc/trip/model/cells/Movable.java
@@ -0,0 +1,15 @@
+package edu.uoc.trip.model.cells;
+
+/**
+ * This interfaces lists the methods that any cell which can be moved must code.
+ *
+ * @author Francesc Dentí
+ * @version 1.0
+ */
+public interface Movable {
+    /**
+     * Moves the cell that invokes this method from the current position/coordinate to the destination coordinate/position.
+     * @param destination Position of the board in which we want to move the cell that invokes this method.
+     */
+    void move(Coordinate destination);
+}
