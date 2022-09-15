3e41ac668ee1ecdde87bc07bd0b83f791e0e69e1
Francesc <francesc.denti@gmail.com>
2022-09-15 10:18:45 +0200

    Implemetación Enum LevelDifficulty

---
+++ src/main/java/edu/uoc/trip/model/levels/LevelDifficulty.java
@@ -0,0 +1,12 @@
+package edu.uoc.trip.model.levels;
+
+/**
+ * Enumeration with the different difficulty levels.
+ */
+public enum LevelDifficulty {
+    STARTER,
+    JUNIOR,
+    EXPERT,
+    MASTER,
+    WIZARD
+}
